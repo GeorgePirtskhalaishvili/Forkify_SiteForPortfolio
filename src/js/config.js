86697ffb96e35f68937ad54c157ee5604147cac5
GeorgePirtskhalaishvili <pirtskhalaishvili.giorgi11@gmail.com>
2022-12-07 03:46:31 +0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUTSEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '918d927c-6a2c-4069-be88-9edd31511092';
+export const MODAL_CLOSE_SEC = 2.5;
